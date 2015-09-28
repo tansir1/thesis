a276be9393feeda943d2ec257744062e6a748111
tansir1 <c_tullock@hotmail.com>
2015-09-27 21:36:38 -0500

    Added bare bones JFrame application window.

---
+++ thesis_gui/src/main/java/thesis/gui/ThesisGUIApp.java
@@ -0,0 +1,13 @@
+package thesis.gui;
+
+import thesis.gui.mainwindow.MainWindow;
+
+public class ThesisGUIApp
+{
+
+   public static void main(String[] args)
+   {
+      MainWindow mainWin = new MainWindow();
+   }
+
+}
---
+++ thesis_gui/src/main/java/thesis/gui/mainwindow/MainWindow.java
@@ -0,0 +1,52 @@
+package thesis.gui.mainwindow;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ * Contains the GUI application's main window frame.
+ *
+ */
+public class MainWindow
+{
+   private JFrame frame;
+   
+   public MainWindow()
+   {
+      frame = new JFrame("Thesis Simulator");
+      frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+      frame.addWindowListener(new WindowAdapter()
+      {
+          @Override
+          public void windowClosing(WindowEvent e)
+          {
+              onClose();
+          }
+      });
+      
+      buildGUI();
+      frame.setVisible(true);
+   }
+   
+   private void buildGUI()
+   {
+      
+   }
+   
+   private void onClose()
+   {
+      //TODO Check if there is data to save
+      //TODO Confirm exit
+      int userChoice = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit Thesis Simulator?",
+            "Confirm exit", JOptionPane.YES_NO_OPTION);
+      if(userChoice == JOptionPane.YES_OPTION)
+      {
+         //TODO Close external resources
+         frame.dispose();
+         System.exit(0);
+      }
+   }
+}
