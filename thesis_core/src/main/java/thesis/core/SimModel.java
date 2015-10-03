5d5dabec54209c4edb1734774a9afd5ee2d42b3c
tansir1 <c_tullock@hotmail.com>
2015-10-03 13:41:50 -0500

    Began stubbing out the sim model and corresponding configuration loader.

---
+++ thesis_core/src/main/java/thesis/core/SimModel.java
@@ -0,0 +1,48 @@
+package thesis.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import thesis.core.utilities.LoggerIDs;
+import thesis.core.utilities.SimModelConfig;
+import thesis.core.world.World;
+
+public class SimModel
+{
+   private Logger logger;
+   private World world;
+
+   /**
+    * Create an uninitialized world.
+    * 
+    * The world must be initialized after being constructed.
+    * 
+    * @see #init(SimModelConfig)
+    */
+   public SimModel()
+   {
+      logger = LoggerFactory.getLogger(LoggerIDs.SIM_MODEL);
+   }
+
+   /**
+    * Initialize the model with the necessary configuration parameters.
+    * 
+    * @param cfg
+    *           Configuration values will be read from this object.
+    */
+   public void init(SimModelConfig cfg)
+   {
+      world = new World(cfg.getWorldWidth(), cfg.getWorldHeight(), cfg.getNumWorldRows(), cfg.getNumWorldCols());
+      logger.info("Sim model initialized with:\n{}", cfg);
+   }
+
+   /**
+    * Get the world map submodel.
+    * 
+    * @return The world map of the simulation.
+    */
+   public World getWorld()
+   {
+      return world;
+   }
+}
