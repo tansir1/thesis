a66245000ae4ae1a0b2547f8e81f0016089d78e1
tansir1 <c_tullock@hotmail.com>
2015-10-10 14:17:30 -0500

    Roads are now generated via KD Tree style mechanism.

---
+++ thesis_core/src/main/java/thesis/core/world/KDTree.java
@@ -0,0 +1,101 @@
+package thesis.core.world;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *A naive KDTree implementation to partition the world space.
+ */
+public class KDTree
+{
+   /**
+    * Utilize the KD Tree algorithm to partition the world space using the given
+    * coordinates as vertices to connect.
+    * 
+    * @param nodes
+    *           The world is partitioned based on these locations.
+    * @return The root node of the generated tree.
+    */
+   public static KDNode generateTree(List<CellCoordinate> nodes)
+   {
+      return generateTree(nodes, 0);
+   }
+
+   private static KDNode generateTree(List<CellCoordinate> nodes, int depth)
+   {
+      if (nodes.isEmpty()) // Occurs when the final leaf element in the tree is
+                           // processed
+      {
+         return null;
+      }
+
+      int axis = depth % 2;// Assumes we only use a cartesian coordinate plane
+
+      Collections.sort(nodes, new CellComparator(axis == 0));
+
+      int median = nodes.size() / 2;
+
+      CellCoordinate location = nodes.get(median);
+
+      KDNode leftChild = null;
+      KDNode rightChild = null;
+
+      if (median != 0) // Occurs when the nodes list contains a single element
+      {
+         leftChild = generateTree(nodes.subList(0, median), depth + 1);
+         rightChild = generateTree(nodes.subList(median + 1, nodes.size()), depth + 1);
+      }
+
+      return new KDNode(location, leftChild, rightChild, axis == 0);
+   }
+
+   private static class CellComparator implements Comparator<CellCoordinate>
+   {
+      private boolean sortVertically;
+
+      public CellComparator(boolean sortVertically)
+      {
+         this.sortVertically = sortVertically;
+      }
+
+      @Override
+      public int compare(CellCoordinate o1, CellCoordinate o2)
+      {
+         int retVal = 0;
+
+         if (sortVertically)
+         {
+            if (o1.getRow() < o2.getRow())
+            {
+               retVal = -1;
+            }
+            else if (o1.getRow() == o2.getRow())
+            {
+               retVal = 0;
+            }
+            else
+            {
+               retVal = 1;
+            }
+         }
+         else
+         {
+            if (o1.getColumn() < o2.getColumn())
+            {
+               retVal = -1;
+            }
+            else if (o1.getColumn() == o2.getColumn())
+            {
+               retVal = 0;
+            }
+            else
+            {
+               retVal = 1;
+            }
+         }
+         return retVal;
+      }
+
+   }
+}
