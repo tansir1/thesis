e5be34587d898c0f7138297e6d51a7a76ef39f3f
tansir1 <c_tullock@hotmail.com>
2015-10-26 21:32:25 -0500

    Dissertation compile script now invokes pandoc.

--- dissertation/compile.py
+++ dissertation/compile.py
@@ -1,4 +1,9 @@
 import argparse
+import subprocess
+
+sources = ['text/doc_root.md',
+           'text/title.md',
+           'text/intro.md']
 
 
 def pdf():
@@ -7,17 +12,44 @@ def pdf():
     """
     print("Generating pdf output")
 
+    cmds = ['pandoc']
+    cmds.extend(sources)
+
+    options = ['-s', '-o', './bin/thesis.pdf']
+    cmds.extend(options)
+
+    print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+
 def html():
     """
     Converts the Markdown files into a stand alone html site via Pandoc.
     """
     print("Generating html output")
 
+    cmds = ['pandoc']
+    cmds.extend(sources)
+
+    options = ['-s', '-o', './bin/thesis.html']
+    cmds.extend(options)
+
+    print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+
 def docx():
     """
     Converts the Markdown files into MS Word docx format via Pandoc.
     """
     print("Generating docx output")
+
+    cmds = ['pandoc']
+    cmds.extend(sources)
+
+    options = ['-s', '-o', './bin/thesis.docx']
+    cmds.extend(options)
+
+    print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
 
 def main():
     parser = argparse.ArgumentParser(description="Compile the Markdown files into an output format via Pandoc.")
@@ -33,6 +65,7 @@ def main():
         pdf()
     elif args.docx:
         docx()
+
 
 if __name__ == "__main__":
     main()
