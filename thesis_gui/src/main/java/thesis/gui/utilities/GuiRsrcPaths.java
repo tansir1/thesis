b544a55c7a9e4631a9ae66f7801aeafa7692dd06
tansir1 <c_tullock@hotmail.com>
2015-11-29 14:14:02 -0600

    Added 50x and 100x playback speeds.

--- thesis_gui/src/main/java/thesis/gui/utilities/GuiRsrcPaths.java
+++ thesis_gui/src/main/java/thesis/gui/utilities/GuiRsrcPaths.java
@@ -9,6 +9,8 @@ public class GuiRsrcPaths
 	public static String RUN_5X_IMG_PATH = "thesis/gui/run5x.png";
 	public static String RUN_10X_IMG_PATH = "thesis/gui/run10x.png";
 	public static String RUN_20X_IMG_PATH = "thesis/gui/run20x.png";
+	public static String RUN_50X_IMG_PATH = "thesis/gui/run50x.png";
+	public static String RUN_100X_IMG_PATH = "thesis/gui/run100x.png";
 	public static String PAUSE_IMG_PATH = "thesis/gui/pause.png";
 	public static String STEP_IMG_PATH = "thesis/gui/step.png";
 }
