393adc2cdf676e047c786a2d279b8cd18908a9b3
tansir1 <c_tullock@hotmail.com>
2015-12-05 17:07:52 -0600

    Basic sensor logic updates per frame.

--- thesis_core/src/main/java/thesis/core/entities/uav/sensors/SensorGroup.java
+++ thesis_core/src/main/java/thesis/core/entities/uav/sensors/SensorGroup.java
@@ -1,6 +1,43 @@
 package thesis.core.entities.uav.sensors;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import thesis.core.common.WorldCoordinate;
+
 public class SensorGroup
 {
+   private List<Sensor> sensors;
 
+   public SensorGroup()
+   {
+      sensors = new ArrayList<Sensor>();
+   }
+
+   public void addSensor(SensorType type)
+   {
+      sensors.add(new Sensor(type));
+   }
+
+   public List<Sensor> getSensors()
+   {
+      return Collections.unmodifiableList(sensors);
+   }
+
+   public void stepSimulation(WorldCoordinate hostUAVLocation)
+   {
+      for(Sensor s : sensors)
+      {
+         s.stepSimulation(hostUAVLocation);
+      }
+   }
+
+   public void stareAtAll(WorldCoordinate starePoint)
+   {
+      for(Sensor s : sensors)
+      {
+         s.slewToLookAt(starePoint);
+      }
+   }
 }
