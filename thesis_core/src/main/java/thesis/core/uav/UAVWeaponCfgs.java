b5fe9dd825a1ca6f9b339ef8fae2a4ef6cf08fc1
tullock <c_tullock@hotmail.com>
2016-01-27 20:24:03 -0600

    Added UAV/Weapons loadout config.

---
+++ thesis_core/src/main/java/thesis/core/uav/UAVWeaponCfgs.java
@@ -0,0 +1,40 @@
+package thesis.core.uav;
+
+public class UAVWeaponCfgs
+{
+
+   public UAVWeaponCfgs()
+   {
+
+   }
+
+   public void reset(int numUAVTypes, int numWpnTypes)
+   {
+      // TODO Implement me
+   }
+
+   public void addWeaponToUAV(int uavType, int wpnType, int initialQty)
+   {
+      // TODO Implement me
+   }
+
+   public boolean uavHasWeapon(int uavType, int wpnType)
+   {
+      // TODO Implement me
+      return false;
+   }
+
+   public int getInitialQuantity(int uavType, int wpnType)
+   {
+      // TODO Implement me
+      return 0;
+   }
+   /*
+
+   private static class WpnQty
+   {
+      public int wpnType;
+      public int initQty;
+   }
+   */
+}
