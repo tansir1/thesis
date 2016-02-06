e7d74f88237c283003a5c566ec540536496d7159
tansir1 <c_tullock@hotmail.com>
2016-02-06 14:19:18 -0600

    The standalone sim app now runs.

--- thesis_sim/src/main/java/thesis/sim/ThesisSimApp.java
+++ thesis_sim/src/main/java/thesis/sim/ThesisSimApp.java
@@ -1,62 +1,152 @@
 package thesis.sim;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import thesis.core.SimModel;
+import thesis.core.common.SimTime;
+import thesis.core.utilities.LoggerIDs;
 import thesis.network.ClientComms;
 import thesis.network.messages.InfrastructureMsg;
-import thesis.network.messages.TestMsg;
+import thesis.network.messages.SimTimeMsg;
+import thesis.sim.utilities.SimAppConfig;
 
 public class ThesisSimApp
 {
+   /**
+    * If the time between now and the last network processing is greater than
+    * this value (milliseconds) then process the network communications.
+    */
+   private final long NETWORK_INTERVAL_MS = 500;
 
-   public static void main(String[] args) throws InterruptedException
+   private Logger logger = LoggerFactory.getLogger(LoggerIDs.MAIN);
+
+   private SimModel simModel;
+   private ClientComms network;
+   private volatile boolean terminateApp;
+   private boolean pause;
+
+   private long lastNetworkTime;
+   private long frameCnt;
+
+   public ThesisSimApp()
    {
-      // TODO Auto-generated method stub
-      // load configs
-      // Init sim
-      // listen for gui to connect
+      network = new ClientComms();
+      terminateApp = false;
+      pause = false;
 
-      ClientComms comms = new ClientComms();
-      if (comms.connect("127.0.0.1", 10555))
+      lastNetworkTime = 0;
+      frameCnt = -1;
+   }
+
+   public boolean init(SimAppConfig cfg, SimModel simModel)
+   {
+      boolean success = true;
+
+      if (cfg.isEnableNetwork())
       {
-         int data[] = new int[3];
-         data[0] = 0;
-         data[1] = 1;
-         data[2] = 2;
+         success = network.connect(cfg.getServerIP(), cfg.getServerPort());
+      }
 
-         TestMsg sendMsg1 = new TestMsg();
-         sendMsg1.setData(data);
+      this.simModel = simModel;
 
-         data[0] = 3;
-         data[1] = 4;
-         data[2] = 5;
-         TestMsg sendMsg2 = new TestMsg();
-         sendMsg2.setData(data);
+      return success;
+   }
 
-         List<InfrastructureMsg> msgs = new ArrayList<InfrastructureMsg>();
-         msgs.add(sendMsg1);
-         msgs.add(sendMsg2);
-         comms.sendData(msgs);
+   public void terminateSim()
+   {
+      terminateApp = true;
+   }
 
-         do
+   private void processIncomingMessages()
+   {
+      if(!network.isReady())
+      {
+         return;
+      }
+
+      logger.trace("Processing data from server");
+      for (InfrastructureMsg msg : network.getData())
+      {
+         switch (msg.getMessageType())
          {
-            msgs = comms.getData();
-            Thread.sleep(500);
-         } while (msgs.isEmpty());
-
-         TestMsg recvMsg = (TestMsg)msgs.get(0);
-         data = recvMsg.getData();
-         for(int i=0; i<data.length; ++i)
-         {
-            System.out.println(data[i]);
+         default:
+            logger.warn("No handlers exist for messages of type {}.", msg.getMessageType());
+            break;
          }
       }
-      else
+   }
+
+   private void transmitPeriodicStatus()
+   {
+      if(!network.isReady())
       {
-         System.err.println("Could not connect.");
+         return;
       }
-      comms.disconnect();
+
+      logger.trace("Transmitting data to server");
+      SimTimeMsg simTimeMsg = new SimTimeMsg();
+      simTimeMsg.setFrameCount(frameCnt);
+      simTimeMsg.setSimTime(SimTime.CURRENT_SIM_TIME_MS);
+      simTimeMsg.setSimWallTime(SimTime.getWallTime());
+
+
+   }
+
+   public void runSim()
+   {
+      long wallTime = 0;
+      boolean processNetwork = false;
+
+      long lastUpdateTime = 0;
+      long frameTime = 0;
+      while (!terminateApp)
+      {
+         wallTime = System.currentTimeMillis();
+         frameCnt++;
+
+         if ((wallTime - lastNetworkTime) > NETWORK_INTERVAL_MS)
+         {
+            lastNetworkTime = wallTime;
+            processNetwork = true;
+            processIncomingMessages();
+         }
+
+         if (!pause)
+         {
+            simModel.stepSimulation();
+         }
+
+         if (processNetwork)
+         {
+            transmitPeriodicStatus();
+            processNetwork = false;
+         }
+
+         wallTime = System.currentTimeMillis();
+
+         frameTime = wallTime - lastUpdateTime;
+         lastUpdateTime = wallTime;
+
+         // Compute how much time is left from now until the next scheduled
+         // frame and sleep.
+         long remainingStepTime = SimTime.SIM_STEP_RATE_MS - frameTime;
+         if (remainingStepTime > 0)
+         {
+            try
+            {
+               Thread.sleep(remainingStepTime);
+            }
+            catch (InterruptedException e)
+            {
+               logger.error("Failed to sleep and delay for the remaining steady frame time.  Details: {}",
+                     e.getMessage());
+            }
+         }
+      }
+
+      logger.info("Terminating simulation.");
+      network.disconnect();
    }
 
 }
