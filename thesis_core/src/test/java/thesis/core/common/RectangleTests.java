6aa510d8a808b405d2961a2d362be687632f582c
tullock <c_tullock@hotmail.com>
2016-02-13 15:02:07 -0600

    Added ability to convert Rectangle data to cannonical form.

--- thesis_core/src/test/java/thesis/core/common/RectangleTests.java
+++ thesis_core/src/test/java/thesis/core/common/RectangleTests.java
@@ -14,12 +14,13 @@ public class RectangleTests
    {
       Rectangle testMe = new Rectangle();
 
-      /*
-       * 0,10-----------30,10
+      /* @formatter:off
+       * 10,0-----------10,30
        *  |               |
-       * 0,0------------30,0
+       * 0,0------------0,30
+       * @formatter:on
        */
-      testMe.getBottomLeft().setCoordinate(0,0);
+      testMe.getBottomLeft().setCoordinate(0, 0);
       testMe.getTopLeft().setCoordinate(10, 0);
       testMe.getTopRight().setCoordinate(10, 30);
       testMe.getBottomRight().setCoordinate(0, 30);
@@ -30,7 +31,7 @@ public class RectangleTests
       WorldCoordinate testPt = new WorldCoordinate(100, 100);
       assertFalse("Point outside of rectangle marked as inside.", testMe.isCoordinateInRegion(testPt));
 
-      testPt.setCoordinate(5,5);
+      testPt.setCoordinate(5, 5);
       assertTrue("Point inside of rectangle marked as outside.", testMe.isCoordinateInRegion(testPt));
    }
 
@@ -47,8 +48,45 @@ public class RectangleTests
       WorldCoordinate testPt = new WorldCoordinate(100, 100);
       assertFalse("Point outside of rectangle marked as inside.", testMe.isCoordinateInRegion(testPt));
 
-      testPt.setCoordinate(3,4);
+      testPt.setCoordinate(3, 4);
       assertTrue("Point inside of rectangle marked as outside.", testMe.isCoordinateInRegion(testPt));
 
+   }
+
+   @Test
+   public void canonicalFormTest()
+   {
+      /* @formatter:off
+       * We want this...
+       * 10,0-----------10,30
+       *  |               |
+       * 0,0------------0,30
+       * ..but provide this...
+       * 0,30-----------0,0
+       *  |               |
+       * 10,30------------10,0
+       * @formatter:on
+       */
+
+      Rectangle goodRect = new Rectangle();
+      goodRect.getBottomLeft().setCoordinate(0, 0);
+      goodRect.getTopLeft().setCoordinate(10, 0);
+      goodRect.getTopRight().setCoordinate(10, 30);
+      goodRect.getBottomRight().setCoordinate(0, 30);
+
+      //Completely inverted rectange
+      Rectangle badRect = new Rectangle();
+      badRect.getTopLeft().setCoordinate(0, 30);
+      badRect.getTopRight().setCoordinate(0, 0);
+      badRect.getBottomLeft().setCoordinate(10, 30);
+      badRect.getBottomRight().setCoordinate(10, 0);
+
+      badRect.convertToCanonicalForm();
+
+      assertEquals("Failed to convert top left.", badRect.getTopLeft(), goodRect.getTopLeft());
+      assertEquals("Failed to convert top right.", badRect.getTopRight(), goodRect.getTopRight());
+      assertEquals("Failed to convert bottom left.", badRect.getBottomLeft(), goodRect.getBottomLeft());
+      assertEquals("Failed to convert bottom right.", badRect.getBottomRight(), goodRect.getBottomRight());
+      assertTrue("Not in canonical form.", badRect.isCanonicalForm());
    }
 }
