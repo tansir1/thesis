b09ecb7945912467488c70676b8ae4f5065e98e2
tansir1 <c_tullock@hotmail.com>
2016-05-31 12:58:42 -0500

    Refactored pandoc compile script to more easily support pandoc flag changes.

--- dissertation/compile.py
+++ dissertation/compile.py
@@ -9,9 +9,10 @@ sources = ['text/algorithms.md',
            'text/task_alloc.md']
 '''
 
-sources = ['text/dissertation.md']
+sources = ['text/title.md',
+            'text/dissertation.md']
 
-markdown_from = '--from=markdown+fenced_code_blocks'
+markdown_control_flags = ['--from=markdown+fenced_code_blocks', '--toc', '-s']
 
 
 def pdf():
@@ -19,14 +20,8 @@ def pdf():
    Converts the Markdown files into pdf format via Pandoc and pdflatex.
    """
    print("Generating pdf output")
-
-   cmds = ['pandoc']
-   cmds.extend(sources)
-
-   options = [markdown_from, '--to=latex', '-s', '-o', './bin/thesis.pdf']
-   cmds.extend(options)
-   print("Executing: " + ' '.join(cmds))
-   print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+   outputFlags = ['--to=latex', '-o', './bin/thesis.pdf']
+   runPandocCommand(outputFlags)
 
 
 def html():
@@ -34,14 +29,8 @@ def html():
    Converts the Markdown files into a stand alone html site via Pandoc.
    """
    print("Generating html output")
-
-   cmds = ['pandoc']
-   cmds.extend(sources)
-
-   options = [markdown_from, '--to=html', '-s', '-o', './bin/thesis.html']
-   cmds.extend(options)
-
-   print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+   outputFlags = ['--to=html', '-o', './bin/thesis.html']
+   runPandocCommand(outputFlags)
 
 
 def docx():
@@ -49,17 +38,24 @@ def docx():
    Converts the Markdown files into MS Word docx format via Pandoc.
    """
    print("Generating docx output")
+   outputFlags = ['--to=docx', '-o', './bin/thesis.docx']
+   runPandocCommand(outputFlags)
 
+def runPandocCommand(outputFlags):
+   """
+   Executes the Pandoc utility with the global control flags and given output flags.
+   """
    cmds = ['pandoc']
    cmds.extend(sources)
-
-   options = [markdown_from, '--to=docx', '-s', '-o', './bin/thesis.docx']
-   cmds.extend(options)
-
+   cmds.extend(markdown_control_flags)
+   cmds.extend(outputFlags)
+   print("Executing: " + ' '.join(cmds))
    print(subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
 
-
 def main():
+   if(not os.path.isdir('./bin')):
+      os.mkdir('./bin')   
+
    parser = argparse.ArgumentParser(description="Compile the Markdown files into an output format via Pandoc.")
    build_type = parser.add_mutually_exclusive_group(required=True)
    build_type.add_argument("--all", action="store_true", help="Generate all supported formats.")
