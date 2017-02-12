a736bba4bc56308213db4864e9540ba8da4fe52a
tansir1 <c_tullock@hotmail.com>
2017-02-11 18:58:03 -0600

    More draft 3 feedback from marques. Added section on johnson criteria to target model.

--- dissertation/convertToDocx.sh
+++ dissertation/convertToDocx.sh
@@ -1,3 +1,3 @@
 #Pandoc cannot find images in a subfolder, so everything in diagrams have to be moved up temporarily.
-pandoc dissertation.tex --to=docx -o thesis.docx
+pandoc dissertation.tex --to=docx -o dissertation.docx
 
